39563b65e9fec65ee58258d781e0d48a894041dc
s.dipaolo90 <s.dipaolo90@gmail.com>
2025-11-19 17:34:01 +0000

    Updated and fixed the API ENDPOINT for Gemini

--- scripts/generate_docs.py
+++ scripts/generate_docs.py
@@ -15,10 +15,10 @@ API_KEY = os.getenv("GEMINI_API_KEY")
 BOT_API_KEY = os.getenv("BOT_API_KEY")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-GEMINI_API_ENDPOINT = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent"
+GEMINI_API_ENDPOINT = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent"
 SRC_FOLDER = "src/"
 DOCS_DOTX_FOLDER = "docs/dotx/components/"
-TEMPLATE_PATH = "docs/dotx/template_document.dotx"
+TEMPLATE_PATH = "docs/dotx/template_documento.dotx"
 
 GIT_BOT_EMAIL = "actions-bot@github.com"
 GIT_BOT_NAME = "GitHub Actions Bot"
